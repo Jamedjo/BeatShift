d3843cb0da2b8231cbaea820803e1891e8439451
Ian <il8998@bristol.ac.uk>
2011-05-11 20:35:38 +0100

    Garbage Collection.

--- GameCode/Utilities & Misc/SoundTrack.cs
+++ GameCode/Utilities & Misc/SoundTrack.cs
@@ -140,9 +140,11 @@ namespace BeatShift
              mpb = 60000.0m/bpm;
              soundBank = new SoundBank(BeatShift.engine, "Content\\XACT\\Map1.xsb");
              waveBank = new WaveBank(BeatShift.engine, "Content\\XACT\\Map1.xwb",0,32);
-            effectWave = new WaveBank(BeatShift.engine,"Content\\XACT\\SoundEffects.xwb");
+             effectWave = new WaveBank(BeatShift.engine,"Content\\XACT\\SoundEffects.xwb");
              musicCategory = BeatShift.engine.GetCategory("Music");
              track = soundBank.GetCue(currentTrack);
+
+             GC.Collect();
 
              while (!track.IsPrepared)
              {
