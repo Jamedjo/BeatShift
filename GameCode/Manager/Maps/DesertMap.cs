ed43316bbfaf1f65d4ea4af0838391805775bdcb
Jamie <jd8489@bristol.ac.uk>
2011-05-15 23:14:00 +0100

    Desert map in textured City updated

--- GameCode/Manager/Maps/DesertMap.cs
+++ GameCode/Manager/Maps/DesertMap.cs
@@ -23,27 +23,6 @@ namespace BeatShift
             modelList.Add(new FbxModel("DesertMap_scenery", MapContent, MapName.DesertMap, ModelCategory.Scenery));
             mapData= new MapData("DesertMap", 60f, 4f);
 
-            //getting ready for all the scenery objects to be individual FBXs with UV maps
-            //modelList.Add(new FbxModel("Desert\banner_pole", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\camera_stand", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\circle_sign", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\diamond_sign", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\double_board", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\hanger", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\lampost", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\large_darrow", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\light_stand", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\med1_board", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\med2_board", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\small_board", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\small_darrow", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\square_sign", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\start_board", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\support", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\transmitter", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\wide_board", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-            //modelList.Add(new FbxModel("Desert\wind_turbine", MapContent, MapName.DesertMap, ModelCategory.Scenery));
-
             mapTrackTexture = BeatShift.contentManager.Load<Texture2D>("Textures/Track2_Diffuse");
             mapTrackAlphaTexture = BeatShift.contentManager.Load<Texture2D>("Textures/Track2_Alpha");
             mapTrackNormalTexture = BeatShift.contentManager.Load<Texture2D>("Textures/Track2_Normal");
