a847ba441c82c154df3f81029d12ecde53fe57a9
James E-J <jamedjo@gmail.com>
2011-05-26 02:50:13 +0100

    Created ProfileSection class to simplify DebugTools-profiling

---
+++ GameCode/GameDebugTools/ProfileSection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace BeatShift.GameDebugTools
+{
+    struct ProfileSection : IDisposable
+    {
+        
+#if DEBUG
+        private String SectionName;
+#endif
+        public ProfileSection(String sectionName, Color rulerColour)//,TimeRuler rulerToUse)
+        {
+#if DEBUG
+            this.SectionName = sectionName;
+            DebugSystem.Instance.TimeRuler.BeginMark(SectionName,rulerColour);
+#endif
+        }
+
+        public void Dispose()
+        {
+#if DEBUG
+            DebugSystem.Instance.TimeRuler.EndMark(SectionName);
+#endif
+        }
+    }
+}
